b76c66916f59836d69e05a2b222d52bd2a996813
cun3 <nachiketh1989@gmail.com>
2016-05-05 00:40:07 -0400

    Added new solution for Pangram problem

---
+++ AlgorithmsSolution/AlgorithmsSolution/Pangram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolution
+{
+        class Pangram
+        {
+            static void Main(string[] args)
+            {
+                string inputStr = Console.ReadLine();
+                SortedDictionary<char, int> letterDict = new SortedDictionary<char, int>();
+
+                inputStr = inputStr.Replace(" ", "");
+
+                if (inputStr.Length < 26)
+                {
+                    Console.WriteLine("not pangram");
+
+                }
+
+                foreach (char c in inputStr.ToLower())
+                {
+                    if (letterDict.ContainsKey(c))
+                    {
+                        letterDict[c]++;
+                    }
+                    else
+                    {
+                        letterDict.Add(c, 1);
+                    }
+                }
+
+                if (letterDict.Count == 26)
+                {
+                    Console.WriteLine("pangram");
+                }
+                else
+                {
+                    Console.WriteLine("not a pangram");
+                }
+            }
+        }
+    }
