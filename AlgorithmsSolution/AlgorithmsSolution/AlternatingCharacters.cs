e710268be0d1bc1530160acd49c976d1ca595128
cun3 <nachiketh1989@gmail.com>
2016-05-12 19:41:50 -0400

    Added code for Paranthesis Balancing

--- AlgorithmsSolution/AlgorithmsSolution/AlternatingCharacters.cs
+++ AlgorithmsSolution/AlgorithmsSolution/AlternatingCharacters.cs
@@ -9,56 +9,56 @@ namespace AlgorithmsSolution
 {
     class AlternatingCharacters
     {
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
-            string s1 = Console.ReadLine();
-            int n = Convert.ToInt16(s1);
+        //    string s1 = Console.ReadLine();
+        //    int n = Convert.ToInt16(s1);
 
-            for (int i = 0; i < n; i++)
-            {
-                string s = Console.ReadLine();
-                Console.WriteLine(DeletionCount(s));
-            }
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        string s = Console.ReadLine();
+        //        Console.WriteLine(DeletionCount(s));
+        //    }
 
-        }
+        //}
 
-        public static int DeletionCount(string strInput)
-        {
-            int noOfDeletion = 0;
+        //public static int DeletionCount(string strInput)
+        //{
+        //    int noOfDeletion = 0;
 
-            char first = strInput[0];
+        //    char first = strInput[0];
 
-            strInput = strInput.Substring(1);
-            foreach (char c in strInput)
-            {
-                if (first == 'A')
-                {
-                    if (c == 'B')
-                    {
-                        first = 'B';
-                    }
-                    else
-                    {
-                        noOfDeletion++;
-                    }
-                }
-                else if (first == 'B')
-                {
-                    if (c == 'A')
-                    {
-                        first = 'A';
-                    }
-                    else
-                    {
-                        noOfDeletion++;
-                    }
-                }
+        //    strInput = strInput.Substring(1);
+        //    foreach (char c in strInput)
+        //    {
+        //        if (first == 'A')
+        //        {
+        //            if (c == 'B')
+        //            {
+        //                first = 'B';
+        //            }
+        //            else
+        //            {
+        //                noOfDeletion++;
+        //            }
+        //        }
+        //        else if (first == 'B')
+        //        {
+        //            if (c == 'A')
+        //            {
+        //                first = 'A';
+        //            }
+        //            else
+        //            {
+        //                noOfDeletion++;
+        //            }
+        //        }
 
 
-            }
-            return noOfDeletion;
-        }
+        //    }
+        //    return noOfDeletion;
+        //}
 
     }
 
---
+++ AlgorithmsSolution/AlgorithmsSolution/ParanthesisBalancing.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsSolution
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            string s1 = Console.ReadLine();
+            int n = Convert.ToInt16(s1);
+
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(BalanceParanthesis(s));
+            }
+        }
+
+        public static string BalanceParanthesis(string s)
+        {
+
+            Stack stkPar = new Stack();
+            string str = "";
+            foreach (char c in s)
+                {
+                    if(c == '{' || c== '(' || c == '[')
+                    {
+                        stkPar.Push(c);
+                    }
+
+                    if(c == '}' && stkPar.Count != 0 )
+                    {
+                        str= stkPar.Pop().ToString();
+                        if (str != "{")
+                            return "FALSE";
+                    }
+                    else if (c == ')' && stkPar.Count != 0)
+                    {
+                        str = stkPar.Pop().ToString();
+                        if (str != "(")
+                            return "FALSE";
+                    }
+                    else if (c == ']' && stkPar.Count != 0)
+                    {
+                        str = stkPar.Pop().ToString();
+                        if (str != "[")
+                            return "FALSE";
+                    }
+                                       
+                }
+
+            if (stkPar.Count == 0)
+                return "TRUE";
+            else
+                return "FALSE";
+            
+        }
+    }
+}
